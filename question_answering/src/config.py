7f0b1e8994ece1d7e153f211209531c0bdf13bd5
Alex Thewsey <athewsey@gmail.com>
2020-05-04 05:14:12 +0000

    feat(qna): WIP training script and nb

--- question_answering/src/config.py
+++ question_answering/src/config.py
@@ -6,20 +6,106 @@ import json
 import logging
 import os
 
+# External Dependencies:
+import transformers as txf
+
+
+MODEL_CONFIG_CLASSES = list(txf.MODEL_FOR_QUESTION_ANSWERING_MAPPING.keys())
+MODEL_TYPES = tuple(conf.model_type for conf in MODEL_CONFIG_CLASSES)
+ALL_MODELS = sum((tuple(conf.pretrained_config_archive_map.keys()) for conf in MODEL_CONFIG_CLASSES), (),)
+
+
+def boolean_hyperparam(raw):
+    """Boolean argparse type for convenience in SageMaker
+
+    SageMaker HPO supports categorical variables, but doesn't have a specific type for booleans -
+    so passing `command --flag` to our container is tricky but `command --arg true` is easy.
+    Using argparse with the built-in `type=bool`, the only way to set false would be to pass an
+    explicit empty string like: `command --arg ""`... which looks super weird and isn't intuitive.
+    Using argparse with `type=boolean_hyperparam` instead, the CLI will support all the various
+    ways to indicate 'yes' and 'no' that you might expect: e.g. `command --arg false`.
+    """
+    valid_false = ("0", "false", "n", "no", "")
+    valid_true = ("1", "true", "y", "yes")
+    raw_lower = raw.lower()
+    if raw_lower in valid_false:
+        return False
+    elif raw_lower in valid_true:
+        return True
+    else:
+        raise argparse.ArgumentTypeError(
+        f"'{raw}' value for case-insensitive boolean hyperparam is not in valid falsy "
+        f"{valid_false} or truthy {valid_true} value list"
+    )
+
 
 def parse_args():
     hps = json.loads(os.environ["SM_HPS"])
     parser = argparse.ArgumentParser(description="Train HuggingFace BERT for Q&A")
 
+    ## Network parameters:
+    parser.add_argument("--base-model", type=str, default=hps.get("base-model", "bert-base-uncased")
+        help="Base pre-trained model to fine-tune"
+    )
+    parser.add_argument(
+        "--config-name", type=str,
+        default=hps.get("config-name", None),
+        help=f"Path to pre-trained model or shortcut name selected in the list: {', '.join(ALL_MODELS)}"
+    )
+    parser.add_argument(
+        "--model-type", type=str, default=hps.get("model-type", None),
+        help=f"Model type selected in the list: {', '.join(MODEL_TYPES)}"
+    )
+
     ## Training process parameters:
+    parser.add_argument("--adam-epsilon", type=float, default=hps.get("adam-epsilon", 1e-8),
+        help="Epsilon for Adam optimizer"
+    )
     parser.add_argument("--batch-size", type=int, default=hps.get("batch-size", 4),
         help="Training mini-batch size"
+    )
+    parser.add_argument("--epochs", type=int, default=hps.get("epochs", 10),
+        help="Number of epochs to train for"
+    )
+    parser.add_argument("--grad-acc-steps", type=int, default=hps.get("grad-acc-steps", 1),
+        help="Number of updates steps to accumulate before performing a backward/update pass",
+    )
+    parser.add_argument("--has-unanswerable", type=boolean_hyperparam,
+        default=hps.get("has-unanswerable", False),
+        help="Set true for datasets (e.g. SQuAD v2) with unanswerable questions",
+    )
+    parser.add_argument("--lr", "--learning-rate", type=float,
+        default=hps.get("lr", hps.get("learning-rate", 5e-5)),
+        help="Learning rate (main training cycle)"
+    )
+    parser.add_argument("--max-grad-norm", type=float, default=hps.get("max-grad-norm", 1.0),
+        help="Max gradient norm"
+    )
+    parser.add_argument("--seed", "--random-seed", type=int,
+        default=hps.get("seed", hps.get("random-seed", None)),
+        help="Random seed fixed for reproducibility (off by default)"
+    )
+    parser.add_argument("--warmup-steps", type=int, default=hps.get("warmup-steps", 0),
+        help="Linear warmup over warmup_steps."
+    )
+    parser.add_argument("--wd", "--weight-decay", type=float,
+        default=hps.get("wd", hps.get("weight-decay", 0.0)),
+        help="Weight decay (if any)"
+    )
+
+    # Resource Management:
+    parser.add_argument("--num-gpus", type=int, default=os.environ.get("SM_NUM_GPUS", 0),
+        help="Number of GPUs to use in training."
     )
 
     # I/O Settings:
     parser.add_argument("--log-level", default=hps.get("log-level", logging.INFO),
         help="Log level (per Python specs, string or int)."
     )
+    parser.add_argument("--model-dir", type=str,
+        default=os.environ.get("SM_MODEL_DIR", "/opt/ml/model")
+    )
+    parser.add_argument("--train", type=str, default=os.environ.get("SM_CHANNEL_TRAIN"))
 
     args = parser.parse_args()
 
--- question_answering/src/train.py
+++ question_answering/src/train.py
@@ -2,10 +2,12 @@
 
 # Python Built-Ins:
 import logging
+import random
 
 # External Dependencies:
+import numpy as np
 import torch
-import transformers
+import transformers as txf
 
 # Local Dependencies:
 import config
@@ -14,8 +16,66 @@ import config
 logger = logging.getLogger("train")
 
 
+def set_seed(args):
+    if args.seed:
+        random.seed(args.seed)
+        np.random.seed(args.seed)
+        torch.manual_seed(args.seed)
+        if args.num_gpus > 0:
+            torch.cuda.manual_seed_all(args.seed)
+
+
+def save_progress(model, args):
+    """Save the model and associated tokenizer"""
+    logger.info("Saving model checkpoint to %s", args.model_dir)
+
+    # Save a trained model, configuration and tokenizer using `save_pretrained()`.
+    # They can then be reloaded using `from_pretrained()`
+    # Take care of distributed/parallel training
+    model_to_save = model.module if hasattr(model, "module") else model
+    model_to_save.save_pretrained(args.model_dir)
+    tokenizer.save_pretrained(args.model_dir)
+
+    # Good practice: save your training arguments together with the trained model
+    torch.save(args, os.path.join(args.model_dir, "training_args.bin"))
+
+
 def train(args):
+    config = txf.AutoConfig.from_pretrained(args.config_name)
+#     tokenizer = AutoTokenizer.from_pretrained(
+#         args.tokenizer_name if args.tokenizer_name else args.model_name_or_path,
+#         do_lower_case=args.do_lower_case,
+#         cache_dir=args.cache_dir if args.cache_dir else None,
+#     )
+    model = txf.AutoModelForQuestionAnswering.from_pretrained(
+        args.config_name,
+        from_tf=bool(".ckpt" in args.model_name_or_path),
+        config=config,
+    )
+
+    # TODO: Multi-GPU
+    device = torch.device("cuda" if torch.cuda.is_available() and args.num_gpus else "cpu")
+
+    model.to(device)
+
+    # Prepare optimizer and schedule (linear warmup and decay)
+    no_decay = ["bias", "LayerNorm.weight"]
+    optimizer_grouped_parameters = [
+        {
+            "params": [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
+            "weight_decay": args.weight_decay,
+        },
+        {"params": [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], "weight_decay": 0.0},
+    ]
+    optimizer = AdamW(optimizer_grouped_parameters, lr=args.learning_rate, eps=args.adam_epsilon)
+    scheduler = get_linear_schedule_with_warmup(
+        optimizer, num_warmup_steps=args.warmup_steps, num_training_steps=t_total
+    )
+
     logger.info("TODO: Train a model!")
+    return
+
+def evaluate(args, model, tokenizer, prefix=""):
     pass
 
 
@@ -24,13 +84,15 @@ if __name__ == "__main__":
 
     # Set up logger:
     logging.basicConfig()
-    logger = logging.getLogger()
+    logger = logging.getLogger("train")
     try:
         # e.g. convert "20" to 20, but leave "DEBUG" alone
         args.log_level = int(args.log_level)
     except ValueError:
         pass
     logger.setLevel(args.log_level)
+
+    set_seed(args)
 
     # Start training:
     train(args)
