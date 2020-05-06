b8dc3a56ee58f93ebef7d59e8b6b9b4f9b81bfa5
My Nguyen <mynguyen6512@gmail.com>
2020-05-06 10:27:52 +0000

    question_answering inference update type errors

--- question_answering/src/inference.py
+++ question_answering/src/inference.py
@@ -1,32 +1,23 @@
+import logging
 import json
-import numpy as np
-from io import BytesIO
-import torch
 from transformers import AutoTokenizer, AutoConfig, AutoModelForQuestionAnswering, pipeline
-from data import read_examples, get_dataloader
 
 
-def _json_loads(data):
-    stream = BytesIO(data)
-    return json.loads(stream.getvalue().decode())
-
-
-def _json_dumps(data):
-    buffer = BytesIO()
-    buffer.write(json.dumps(data).encode())
-    return buffer.getvalue()
+logger = logging.getLogger()
 
 
 def input_fn(input_bytes, content_type):
     if content_type == 'application/json':
-        return _json_loads(input_bytes)
+        logger.info(input_bytes)
+        return json.loads(input_bytes)
     else:
         raise ValueError('Content type must be application/json')
 
 
 def output_fn(prediction_output, accept):
     if accept == 'application/json':
-        return _json_dumps(prediction_output), 'application/json'
+        logger.info(prediction_output)
+        return prediction_output, 'application/json'
     else:
         raise ValueError('Accept header must be application/json')
 
@@ -40,7 +31,7 @@ def model_fn(model_dir):
 
 def predict_fn(input_data, model):
     trained_model, tokenizer, config = model
-    nlp = pipeline(task="question-answering", model=model, config=config, tokenizer=tokenizer, framework="pt")
+    nlp = pipeline(task="question-answering", model=trained_model, config=config, tokenizer=tokenizer, framework="pt")
     result = nlp(input_data)
     return result
     
